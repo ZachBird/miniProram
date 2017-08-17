1d97145130ac5f12263d0954c525fed665b3081e
可囿可无 <546439325@qq.com>
2017-08-17 18:12:40 +0800

    8.17 update

---
+++ test_demo/pages/category/category.js
@@ -0,0 +1,7 @@
+// category.js
+Page({
+  items: [
+    {one: "111"},
+    {one: "2222"}
+  ]
+})
--- test_demo/pages/index/index.js
+++ test_demo/pages/index/index.js
@@ -1,26 +1,35 @@
 //index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    imgUrls: [
+      {url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'},
+      {url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg'},
+      {url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'}
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    circular: true
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
     })
   }
 })
